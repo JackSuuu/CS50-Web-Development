7dc61df066a11df0b69207822d675579997500eb
Jack <jack@JACKs-MacBook-Pro.local>
2024-06-02 17:30:13 +0800

    first commit

--- CS50_Web_Dev/jack_toDoist/urls.py
+++ CS50_Web_Dev/jack_toDoist/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+# This is how we use as a variable in Django HTML
+# For example: we name tasks:index to redirect the link back
 app_name = "tasks"
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("add", views.add, name="add")
+    path("add/", views.add, name="add")
 ]
--- CS50_Web_Dev/jack_toDoist/views.py
+++ CS50_Web_Dev/jack_toDoist/views.py
@@ -1,13 +1,43 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-tasks = ["lundary", "shopping", "study"]
+# tasks = ["dinner", "grocery"]
+
+# A class to represent the form
+class NewTaskForm(forms.Form):
+    # Define the form as inputing character
+    task = forms.CharField(label="New Task")
+    # Define the priority as integer
+    # priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
+
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     # A very often seeing pattern in django
     # Create a variable and implement logic in the web pages
     return render(request, "index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 
 def add(request):
-    return render(request, "add.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        # if the form is valid, we take the data from the form
+        # get the task, add that into our list named tasks
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            request.session["tasks"] += [task]
+            # adding a redirect link back to our orignal page
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            # if the form is not valid, we can display any error that come out
+            return render(request, "add.html", {
+                "form": form
+            })
+
+    return render(request, "add.html", {
+        "form": NewTaskForm()
+    })
